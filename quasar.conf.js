5b875e17eaa5c0f6e54b3938b549cf92cbc8dbf4
Tomer Shalev <tshalev@proofpoint.com>
2020-03-04 17:05:46 +0200

    Add language selection persistency

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,6 +48,7 @@ module.exports = function (ctx) {
 
       // Quasar plugins
       plugins: [
+        'Cookies',
         'Notify'
       ]
     },
--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -1,6 +1,6 @@
 <template>
   <q-select
-    v-if="lang"
+    v-if="isShown"
     v-model="lang"
     :options="langOptions"
     bg-color="grey"
@@ -18,6 +18,7 @@
 import { availableLangs } from '../../i18n.config'
 import languages from 'quasar/lang/index.json'
 
+const LANG_COOKIE = 'lang'
 let appLanguages = languages
 if (availableLangs) {
   appLanguages = appLanguages.filter(lang =>
@@ -32,33 +33,51 @@ export default {
       langOptions: []
     }
   },
+  computed: {
+    isLangInAppLangs () {
+      return appLanguages && this.lang &&
+        appLanguages.map(lang => lang.isoName).includes(this.lang)
+    },
+    isShown () {
+      return this.isLangInAppLangs
+    }
+  },
   watch: {
     lang () {
       // dynamic import, so loading on demand only
       import(
         /* webpackInclude: /(he|en-us)\.js$/ */
         `quasar/lang/${this.lang}`
-      ).then(lang => {
-        this.$q.lang.set(lang.default)
-        this.$i18n.locale = lang.default.isoName
-      })
+      )
+        .then(lang => {
+          this.$q.lang.set(lang.default)
+          this.$i18n.locale = lang.default.isoName
+          this.$q.cookies.set(LANG_COOKIE, lang.default.isoName)
+        })
+        .catch(() => {
+          console.error(`Failed to load language ${this.lang}`)
+          if (appLanguages) {
+            // set to first available language
+            this.lang = appLanguages[0].isoName
+          }
+        })
     }
   },
   created () {
-    // set language according to quasar's framework.lang variable, as configured
-    // in quasar.conf.js
-    const frameworkLang = this.$q.lang.isoName
-
-    if (!appLanguages.map(lang => lang.isoName).includes(frameworkLang)) {
-      // do not use this component when framework's language is not in the list
-      // of available languages
+    if (!appLanguages) {
       return
     }
-    this.lang = frameworkLang
+
+    // set language according to quasar's framework.lang variable, as configured
+    // in quasar.conf.js
+    const savedLang = this.$q.cookies.get(LANG_COOKIE)
+    const frameworkLang = this.$q.lang.isoName
+
+    this.lang = savedLang || frameworkLang
 
     // populate language selector
-    this.langOptions = appLanguages.map(lang => ({
-      label: lang.nativeName, value: lang.isoName
+    this.langOptions = appLanguages.map(langObj => ({
+      label: langObj.nativeName, value: langObj.isoName
     }))
   }
 }
